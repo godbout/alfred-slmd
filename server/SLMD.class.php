252cb13b38b61b770ee235c9f52fb25d37ea61d5
Guillaume Leclerc <guill.bout@gmail.com>
2017-01-04 00:21:17 +0800

    Add loading settings + create writing url and pic url on the fly

--- server/SLMD.class.php
+++ server/SLMD.class.php
@@ -4,20 +4,28 @@ class SLMD
 {
     protected $msqli = null;
     protected $writingId = 0;
+    protected $settings = null;
 
     public function __construct()
     {
         $this->mysqli = new mysqli('localhost', 'post.slmd.info', 'post.slmd.info', 'post.sleeplessmind.info');
+
+        $this->loadSettings();
+    }
+
+    public function loadSettings()
+    {
+        $result = $this->mysqli->query('SELECT name, value FROM settings');
+        $settings = $result->fetch_all(MYSQLI_ASSOC);
+        foreach ($settings as $key => $setting) {
+            $this->settings[$setting['name']] = $setting['value'];
+        }
+        $result->free();
     }
 
     public function getToken()
     {
-        $result = $this->mysqli->query('SELECT value FROM settings WHERE name = \'token\'');
-        $row = $result->fetch_assoc();
-        $token = $row['value'];
-        $result->close();
-
-        return $token;
+        return $this->settings['token'];
     }
 
     public function getFacebookSettings()
@@ -25,7 +33,7 @@ class SLMD
         $result = $this->mysqli->query('SELECT value FROM settings WHERE name = \'facebook\'');
         $row = $result->fetch_assoc();
         $facebookSettings = json_decode($row['value'], true);
-        $result->close();
+        $result->free();
 
         return $facebookSettings;
     }
@@ -46,14 +54,30 @@ class SLMD
 
         $facebookData = [
             'message' => $data['message'],
-            'link' => $data['link'],
-            'picture' => $data['picture'],
+            'link' => $this->getWritingUrl($data['title']),
+            'picture' => $this->getWritingPictureUrl($data['title']),
             'name' => $data['title'],
             'caption' => $data['caption'],
             'description' => $data['description'],
         ];
 
         return $facebookData;
+    }
+
+    public function getWritingUrl($writingTitle = '')
+    {
+        $slug = str_replace(' ', '-', strtolower($writingTitle));
+        $slug = preg_replace('/[^A-Za-z0-9\-]/', '', $slug);
+
+        return $this->settings['writings_url'] . $slug . '/';
+    }
+
+    public function getWritingPictureUrl($writingTitle = '')
+    {
+        $pictureName = str_replace(' ', '-', strtolower($writingTitle));
+        $pictureName = preg_replace('/[^A-Za-z0-9\-]/', '', $pictureName);
+
+        return $this->settings['pictures_url'] . $pictureName . '.png';
     }
 
     public function updateWritingStatus()
