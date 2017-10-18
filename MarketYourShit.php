5dd9e447250a415620756dbe1665ac4a2837edef
Guillaume <guill.bout@gmail.com>
2017-10-18 13:08:13 +0800

    Update code with call to new hq.sleeplessmind.info share API

---
+++ MarketYourShit.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+require 'vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\RequestException;
+
+$dotenv = new Dotenv(__DIR__);
+$dotenv->load();
+
+switch ($argv[1]) {
+    case 'all':
+        try {
+            $client = new Client([
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer ' . getenv('API_TOKEN'),
+                ]
+            ]);
+            $response = $client->post(getenv('API_URL'));
+        } catch (RequestException | ConnectException | ClientException $e) {
+            $notification = 'Failed with: ';
+            if ($e->hasResponse()) {
+                $notification .=  $e->getResponse()->getStatusCode();
+                $notification .=  ' ' . $e->getResponse()->getReasonPhrase();
+            }
+
+            echo $notification;
+        }
+        break;
+
+    default:
+        echo 'Single media sharing not supported... yet.';
+        break;
+}
