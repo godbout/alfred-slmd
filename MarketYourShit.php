0ef6ba580be79bb5e815cd02cf1d51e1f43ee6d0
Guillaume <guill.bout@gmail.com>
2017-11-09 02:00:07 +0800

    Add single facebook share + show response body

--- MarketYourShit.php
+++ MarketYourShit.php
@@ -16,29 +16,35 @@ $dotenv->load();
 
 switch ($argv[1]) {
     case 'all':
-        try {
-            $client = new Client([
-                'headers' => [
-                    'Accept' => 'application/json',
-                    'Authorization' => 'Bearer ' . getenv('API_TOKEN'),
-                ]
-            ]);
-            $response = $client->post(getenv('API_URL'));
-        } catch (RequestException | ConnectException | ClientException $e) {
-            $notification = 'Failed with: ';
-            if ($e->hasResponse()) {
-                $notification .=  $e->getResponse()->getStatusCode();
-                $notification .=  ' ' . $e->getResponse()->getReasonPhrase();
-            }
-
-            echo $notification;
-            return;
-        }
-
-        echo 'Succeeded with: ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase();
+    case 'facebook':
+        share($argv[1]);
         break;
 
     default:
         echo 'Single media sharing not supported... yet.';
         break;
+}
+
+function share($service)
+{
+    try {
+        $client = new Client([
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer ' . getenv('API_TOKEN'),
+            ]
+        ]);
+        $response = $client->post(getenv('API_BASEURL') . $service);
+    } catch (RequestException | ConnectException | ClientException $e) {
+        $notification = 'Failed with: ';
+        if ($e->hasResponse()) {
+            $notification .=  $e->getResponse()->getStatusCode();
+            $notification .=  ' ' . $e->getResponse()->getReasonPhrase();
+        }
+
+        echo $notification;
+        return;
+    }
+
+    echo $response->getBody();
 }
